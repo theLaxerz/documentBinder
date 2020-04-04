9e46f44b268a372e6b31848acfd875f53a584618
Aaron F. Lax <aaron.lax@icloud.com>
2020-04-03 20:02:58 -0500

    Code has been updated to start the process right now a header in a flexbox container.

---
+++ src/sidenav/sidenav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sidenav.scss';
+
+function sideNav(){
+    return (
+      <div className="Side-Nav">
+        <button className="Button" href="https://theLaxer.us/">theLaxerz</button>   
+      </div>
+    ); 
+}
+
+export default sideNav
